86016e7e37194a0a368b5d48b9e2a63294d107ce
Karol von Zmuda Trzebiatowski <zhawk83@gmail.com>
2014-08-01 20:10:22 +0000

    * added GPL v2 info + license text

--- FFXEditor/utils.hpp
+++ FFXEditor/utils.hpp
@@ -1,3 +1,24 @@
+/*
+    FFX-Editor is a cheat tool for PCSX2 and Final Fantasy X.
+
+    Copyright (C) 2014  Karl Zmuda Trzebiatowski <karl.zmuda.trzebiatowski at gmail.com>
+
+	This file is part of FFX-Editor.
+
+    FFX-Editor is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FFX-Editor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with FFX-Editor; if not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
